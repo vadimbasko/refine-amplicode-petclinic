eddc74ab1c9aa9bf93df579efb145d938e1d1684
Vlad Minaev <minaev@haulmont.com>
2023-02-02 09:49:42 +0400

    React Buddy ide-toolbox is added

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import "./i18n";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <React.Suspense fallback="loading">
-      <App />
-    </React.Suspense>
-  </React.StrictMode>
+    <React.StrictMode>
+        <React.Suspense fallback="loading">
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
+        </React.Suspense>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
